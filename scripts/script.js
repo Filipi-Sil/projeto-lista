241fef21741daafe5316baa93f21d6e7389f1c5d
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-02-15 18:31:53 -0300

    Update lista

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,48 @@ function fechar(){
     dcriar.style.display = 'none'
 }
 
+const form = document.querySelector("#form");
+const inputNome = document.querySelector("#nome");
+const selectCategoria = document.querySelector("#categoria");
+
+const itens = JSON.parse(localStorage.getItem("lista")) || [];
+itens.forEach(criarItem);
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const item = {
+    nome: inputNome.value,
+    categoria: selectCategoria.value,
+    id: Date.now()
+  };
+
+  itens.push(item);
+  localStorage.setItem("lista", JSON.stringify(itens));
+
+  criarItem(item);
+  form.reset();
+});
+
+function criarItem(item) {
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <label>
+      <input type="checkbox">
+      <span>${item.nome}</span>
+    </label>
+    <button onclick="removerItem(${item.id})">X</button>
+  `;
+
+  document.getElementById(item.categoria).appendChild(div);
+}
+
+function removerItem(id) {
+  const index = itens.findIndex(i => i.id === id);
+  if (index !== -1) {
+    itens.splice(index, 1);
+    localStorage.setItem("lista", JSON.stringify(itens));
+    location.reload(); // jeito simples de atualizar
+  }
+}
